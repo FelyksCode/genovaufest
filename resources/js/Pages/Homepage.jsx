ef04bfc6c55284513b47f064d55091eb15a3ec82
Felyks <gamingfelix78@gmail.com>
2024-02-06 22:45:55 +0700

    Initial commit

---
+++ resources/js/Pages/Homepage.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import { Head } from "@inertiajs/react";
+
+export default function Homepage() {
+    return (
+        <div>
+            <Head title="HomePage" />
+            #isi disini
+        </div>
+    );
+}
