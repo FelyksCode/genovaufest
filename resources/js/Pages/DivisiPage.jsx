34bb5e236f027cec4878de031acdd36210501a95
EdwinLolo <edwinfedora@gmail.com>
2024-02-08 16:00:55 +0700

    dasdasd

--- resources/js/Pages/DivisiPage.jsx
+++ resources/js/Pages/DivisiPage.jsx
@@ -2,10 +2,11 @@ import React from "react";
 import { Head } from "@inertiajs/react";
 import Footer from "@/Components/Footer";
 import CarouselDivisi from "@/Components/divisipage/CarouselDivisi";
+import "./Style.css";
 
 export default function DivisiPage() {
     return (
-        <div className="flex-row">
+        <div className="flex-row badan">
             <Head title="Divisi Page" />
             <div>
                 <CarouselDivisi />
