b8b2e9c82742b6a9cd5d878e5844c016b2615851
EdwinLolo <edwinfedora@gmail.com>
2024-02-27 18:13:59 +0700

    division

--- resources/js/Components/Home/Spinner.jsx
+++ resources/js/Components/Home/Spinner.jsx
@@ -1,57 +1,66 @@
-import React from 'react'
-import '/resources/css/vertical.css'
-import slideimg1 from '/public/Images/Carousels/Logo_Divisi/ALBA - TICKETING.png';
-import slideimg2 from '/public/Images/Carousels/Logo_Divisi/ARDEA - PERLENGKAPAN.png';
-import slideimg3 from '/public/Images/Carousels/Logo_Divisi/CATANIA - PUBLIKASI.png';
-import slideimg4 from '/public/Images/Carousels/Logo_Divisi/FLORENCE - DEKORASI.png';
-import { Swiper, SwiperSlide } from 'swiper/react';
-import 'swiper/css';
-import 'swiper/css/pagination';
-import { Autoplay, Pagination, Navigation } from 'swiper/modules';
+import React, { useState } from "react";
+import "/resources/css/vertical.css";
+import slideimg1 from "/public/Images/Carousels/Logo_Divisi/ALBA - TICKETING.png";
+import slideimg2 from "/public/Images/Carousels/Logo_Divisi/ARDEA - PERLENGKAPAN.png";
+import slideimg3 from "/public/Images/Carousels/Logo_Divisi/CATANIA - PUBLIKASI.png";
+import slideimg4 from "/public/Images/Carousels/Logo_Divisi/FLORENCE - DEKORASI.png";
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/css";
+import "swiper/css/pagination";
+import { Autoplay, Pagination, Navigation } from "swiper/modules";
+import "./spinnerstyle.css";
 
 function Spinner() {
-  const verticalimages = [
-    {image: slideimg1, Text: 'Text1'}, 
-    {image: slideimg2, Text: 'Text2'}, 
-    {image: slideimg3, Text: 'Text3'}, 
-    {image: slideimg4, Text: 'Text4'}
-      ];
+    const [activeIndex, setActiveIndex] = useState(0);
 
+    const verticalimages = [
+        { image: slideimg1, Text: "Text1" },
+        { image: slideimg2, Text: "Text2" },
+        { image: slideimg3, Text: "Text3" },
+        { image: slideimg4, Text: "Text4" },
+    ];
 
-  return (
-    <div className='w-full h-[100vh] bg-vintage'>
-        <div className="backdrop-blur-md bg-white/30 w-full h-full flex justify-center items-center">
-          <Swiper
-              spaceBetween={50}
-              slidesPerView={3}
-              direction='vertical'
-              navigation={true}
-              autoplay={{
-                delay: 2000,
-                disableOnInteraction: false,
-              }}
-              pagination={{
-                clickable: true,
-                dynamicBullets: true,
-              }}
-              centeredSlides = {true}
-              loop = 'true'
-              modules={[Autoplay,Pagination,Navigation]}
-              className="mySwiper"
-              >
-              {verticalimages.map((item, index) => 
-                  <SwiperSlide key={index} className='transition-all duration-3000 hover:blur-none blur cursor-pointer'>
-                    <img src={item.image} alt="" />
-                    <div className='bg-white w-[200px] h-[200px] -my-[205px] flex items-center text-center rounded-3xl ml-[120px] transition-transform hover:-translate-x-[200px] translate-x-0 duration-500'>
-                      <p className=''>{item.Text}</p>
-                    </div>
-                    
-                  </SwiperSlide>
-              )}
-            </Swiper>
+    const handleSlideChange = (swiper) => {
+        setActiveIndex(swiper.realIndex);
+    };
+
+    return (
+        <div className="w-full h-[100vh] bg-vintage">
+            <div className="backdrop-blur-md bg-white/30 w-full h-full flex justify-center items-center">
+                <Swiper
+                    spaceBetween={50}
+                    slidesPerView={3}
+                    direction="vertical"
+                    navigation={true}
+                    pagination={{
+                        clickable: true,
+                        dynamicBullets: true,
+                    }}
+                    centeredSlides={true}
+                    loop={true}
+                    modules={[Autoplay, Pagination, Navigation]}
+                    className="mySwiper"
+                    onSlideChange={handleSlideChange}
+                >
+                    {verticalimages.map((item, index) => (
+                        <SwiperSlide
+                            key={index}
+                            className="transition-all duration-3000 hover:blur-none blur cursor-pointer"
+                        >
+                            <img src={item.image} alt="" />
+                            <div
+                                className={`bg-white w-[200px] h-[200px] -my-[205px] flex items-center text-center rounded-3xl ml-[120px] transition-transform duration-500 ${
+                                    activeIndex === index ? "" : "hidden"
+                                }`}
+                            >
+                                <p className="">{item.Text}</p>
+                            </div>
+                        </SwiperSlide>
+                    ))}
+                </Swiper>
+            </div>
         </div>
-    </div>
-  )
+    );
 }
 
-export default Spinner
+export default Spinner;
