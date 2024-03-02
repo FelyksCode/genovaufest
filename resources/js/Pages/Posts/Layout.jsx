e93ab9e554bbf379de095fed07331731ccfd71b0
EdwinLolo <edwinfedora@gmail.com>
2024-03-02 23:34:54 +0700

    rangkaian

--- resources/js/Pages/Posts/Layout.jsx
+++ resources/js/Pages/Posts/Layout.jsx
@@ -3,6 +3,7 @@ import Alert from "@/Components/Alert/Alert";
 import Tujuan from "@/Components/Tujuan/Tujuan";
 import Navbar from "@/Components/Navbar/Navbar";
 import Divisihome from "@/Components/Home/Divisihome";
+import Rangkaian from "@/Components/Home/Rangkaian";
 import React from "react";
 import { Fade } from "react-reveal";
 
@@ -23,6 +24,11 @@ function Layout() {
                 <section>
                     <Fade>
                         <Divisihome> </Divisihome>
+                    </Fade>
+                </section>
+                <section>
+                    <Fade>
+                        <Rangkaian> </Rangkaian>
                     </Fade>
                 </section>
             </div>
